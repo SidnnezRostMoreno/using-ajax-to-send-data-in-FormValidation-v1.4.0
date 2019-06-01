fe53d9498003944c30ed2aace89822d14de63bc4
SidnnezRostMoreno <contato.comuniquepress@gmail.com>
2019-06-01 00:09:52 -0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,40 @@
+<?php
+
+  header('Content-Type: application/json; charset=utf-8');
+
+  $dataString = file_get_contents('php://input');
+
+  $data = json_decode($dataString);
+
+  $name = strip_tags(trim($_POST['username']));
+
+  $email = strip_tags(trim($_POST['email']));
+
+  $phone = strip_tags(trim($_POST['phone']));
+
+  $realEstate = strip_tags(trim($_POST['realEstate']));
+
+  require_once('PHPMailer/PHPMailerAutoload.php');
+
+  $mail = new PHPMailer();
+
+  // Add your settings ...
+  // See more at: https://github.com/PHPMailer/PHPMailer
+
+  if($mail->Send()) {
+
+      http_response_code(200);
+
+      $response = array ( "result" => "success" );
+
+      echo json_encode($response);
+
+    } else {
+
+      $response = array ( "result" => "error" );
+
+      echo json_encode($response);
+
+    }
+
+ ?>
